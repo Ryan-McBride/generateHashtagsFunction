4db22903984d508ac365ee54ee76dfd1029a52f9
Ryan McBride <Ryan-McBride@users.noreply.github.com>
2019-04-16 23:18:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const words = require('random-words');
 
 exports.handler = async () => {
-  let tags = words(100);
+  let tags = words(30);
   tags = tags.map(item => `#${item}`);
   const response = {
     statusCode: 200,
